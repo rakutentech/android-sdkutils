a8c589b75e08198563a8593dc3359f91174357a7
Caywood, Cory | Cory | ESD <cory.caywood@rakuten.com>
2019-11-18 12:27:00 +0900

    feat: Add application info that is auto-initialized

--- sdk-utils/src/main/kotlin/com/rakuten/tech/mobile/sdkutils/AppInfo.kt
+++ sdk-utils/src/main/kotlin/com/rakuten/tech/mobile/sdkutils/AppInfo.kt
@@ -1,5 +1,44 @@
 package com.rakuten.tech.mobile.sdkutils
 
-class AppInfo {
+import android.content.Context
+import androidx.annotation.VisibleForTesting
 
+/**
+ * Provides information about the App including App name, and version name.
+ * Should be accessed via [AppInfo.instance].
+ */
+abstract class AppInfo {
+
+    /**
+     * Package name of the App.
+     */
+    abstract val name: String
+
+    /**
+     * Version name of the App.
+     */
+    abstract val version: String
+
+    companion object {
+        @JvmStatic
+        lateinit var instance: AppInfo
+
+
+
+        internal fun init(context: Context) {
+            instance = RealAppInfo(context)
+        }
+    }
+}
+
+private data class RealAppInfo @VisibleForTesting constructor(
+    override val name: String,
+    override val version: String
+): AppInfo() {
+
+    constructor(context: Context): this (
+        name = context.packageName,
+        version = context.packageManager
+            .getPackageInfo(context.packageName, 0).versionName
+    )
 }
