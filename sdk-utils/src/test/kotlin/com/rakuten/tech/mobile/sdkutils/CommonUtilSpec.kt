e6026162862df6cd9f5f0affac30bf2b54c34fc1
Md Usman Gani <mdusman.gani@rakuten.com>
2021-12-29 17:06:45 +0900

    Fix failed test case

--- sdk-utils/src/test/kotlin/com/rakuten/tech/mobile/sdkutils/CommonUtilSpec.kt
+++ sdk-utils/src/test/kotlin/com/rakuten/tech/mobile/sdkutils/CommonUtilSpec.kt
@@ -24,6 +24,6 @@ class CommonUtilSpec {
         val simpleDateFormat = SimpleDateFormat("dd.MM.yyyy HH:mm:ss")
         val date = simpleDateFormat.parse("02.04.2014 15:00:00")
         val isoFormattedDate = CommonUtil.getUTCDateFormat().format(date!!)
-        isoFormattedDate shouldBeEqualTo "2014-04-02T15:00:00Z"
+        isoFormattedDate shouldBeEqualTo "2014-04-02T15:00:00"
     }
 }
